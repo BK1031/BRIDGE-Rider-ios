08ae8a405dad5c387a91e0e8e074471a9948d1f5
Bharat Kathi <bharat1031@gmail.com>
2018-07-12 21:44:17 -0700

    Updated Animation

--- BRIDGE-Rider/WelcomeViewController.swift
+++ BRIDGE-Rider/WelcomeViewController.swift
@@ -12,6 +12,8 @@ import Firebase
 class WelcomeViewController: UIViewController {
     
     @IBOutlet weak var getStartedButton: UIButton!
+    @IBOutlet weak var createAccountButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
     
     @IBOutlet weak var registrationView: UIView!
     @IBOutlet weak var registrationViewHeight: NSLayoutConstraint!
@@ -27,12 +29,28 @@ class WelcomeViewController: UIViewController {
         
         getStartedButton.layer.cornerRadius = 10
         registrationView.layer.cornerRadius = 10
+        createAccountButton.layer.cornerRadius = 10
+        createAccountButton.isHidden = true
+        loginButton.isHidden = true
         registrationViewHeight.constant = 0
     }
     
     @IBAction func getStarted(_ sender: UIButton) {
         UIView.animate(withDuration: 0.25, delay: 0, options: [.curveEaseOut], animations: {
             self.registrationViewHeight.constant = self.view.frame.height - (self.view.safeAreaInsets.top + 180)
+            self.view.layoutIfNeeded()
+        }) { (finished) in
+            //Execute once animation finished
+            self.createAccountButton.isHidden = false
+            self.loginButton.isHidden = false
+        }
+    }
+    
+    @IBAction func login(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.25, delay: 0, options: [.curveEaseOut], animations: {
+            self.registrationViewHeight.constant = 0
+            self.createAccountButton.isHidden = true
+            self.loginButton.isHidden = true
             self.view.layoutIfNeeded()
         }) { (finished) in
             //Execute once animation finished
